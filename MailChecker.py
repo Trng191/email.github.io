9c02a6a2bc39e694706daa443dd79c3028483a3b
HandsomeBoy699 <89534486+Begineer17@users.noreply.github.com>
2023-11-05 23:31:29 +0700

    Update MailChecker.py

--- MailChecker.py
+++ MailChecker.py
@@ -1,4 +1,5 @@
 import imaplib
+import time
 import email
 
 server = 'imap.gmail.com'
@@ -9,10 +10,44 @@ imap = imaplib.IMAP4_SSL(host = server, port = 993)
     
 imap.login(account, password)
 
-_, count = imap.select("Inbox")
+#Check mail and implement
+def CheckAndDo(cmd):
+    if(cmd == 'list applications'):
+        print('applications: app A, app B, app C')
+    elif(cmd == 'list processes'):  
+        print('processes: pA, pB, pC')
+    elif(cmd == 'shut down'):  
+        print('shutdown')
+    elif(cmd == 'keylogger'):  
+        print('keylogger: A, B, C, D')
+    elif(cmd == 'screenshot'):  
+        print('photo')
+    else:
+        print(cmd)
 
-print(count[0].decode())
+#Select unseen message in Inbox to read
+cmd = 'start'
+while cmd != 'quit':
+    imap.select("Inbox")
+    res, mailIds = imap.search(None, '(UNSEEN)')
+    print(res)
 
-imap.close()
+    #Try to read email
+    for id in mailIds[0].decode().split():
+        res, mailData = imap.fetch(id, '(RFC822)')
+        message = email.message_from_string(mailData[0][1].decode())
+
+        #Get message
+        for part in message.walk():
+            if(part.get_content_type() == 'text/plain'):
+                cmd = part.as_string().splitlines()[-1]
+                CheckAndDo(cmd)
+                    
+    
+    imap.close()
+    
+    time.sleep(0.75)
+
+print("Byee")
 
 imap.logout()
