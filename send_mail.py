703838ee6b2c09d8f8f01e4e832d65aa4b12abea
ptbtin22 <ptbtin22@clc.fitus.edu.vn>
2023-11-08 10:31:06 +0700

    change theme to lightPurple

--- send_mail.py
+++ send_mail.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 import email_interface
 
-sg.theme('SystemDefault')
+sg.theme('LightPurple')
 
 layout = [
     [sg.Column([[sg.Text("Enter full name:"), sg.Input(key='-NAME-', do_not_clear=True, size=(40, 1))],
